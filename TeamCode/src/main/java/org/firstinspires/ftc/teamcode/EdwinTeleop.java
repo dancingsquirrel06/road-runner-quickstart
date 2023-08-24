bf3a78275bbdbc8a541a20dde9b322ab364a18e4
dancingsquirrel06 <118295532+dancingsquirrel06@users.noreply.github.com>
2023-08-24 11:24:33 -0700

    final - 1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EdwinTeleop.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+@TeleOp
+public class EdwinTeleop extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialize SampleMecanumDrive
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+
+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Retrieve our pose from the PoseStorage.currentPose static field
+        // See AutoTransferPose.java for further details
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive() && !isStopRequested()) {
+            // Read pose
+            Pose2d poseEstimate = drive.getPoseEstimate();
+
+            // Create a vector from the gamepad x/y inputs
+            // Then, rotate that vector by the inverse of that heading
+            Vector2d input = new Vector2d(
+                    -gamepad1.left_stick_y,
+                    -gamepad1.left_stick_x
+            ).rotated(-poseEstimate.getHeading());
+            double driveVelocity = (0.35 + 0.35 * gamepad1.left_trigger);
+
+            // Pass in the rotated input + right stick value for rotation
+            // Rotation is not part of the rotated input thus must be passed in separately
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            input.getX() * driveVelocity,
+                            input.getY() * driveVelocity,
+                            -gamepad1.right_stick_x * driveVelocity
+                    )
+            );
+
+
+            drive.update();
+
+
+            telemetry.addData("x", poseEstimate.getX());
+            telemetry.addData("y", poseEstimate.getY());
+            telemetry.addData("heading", poseEstimate.getHeading());
+            telemetry.update();
+        }
+    }
+}
